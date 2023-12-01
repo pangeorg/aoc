0284bab2c127599aaa75bce4ea4ae8fca182eb92
Georg Stockinger <gstockinger@a2mac1.com>
2023-12-01 12:29:38 +0100

    Day 1: full python solution

--- python/day-01.py
+++ python/day-01.py
@@ -1,4 +1,8 @@
-def find_calibration(s: str):
+
+literal_numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+literal_numbers_values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def find_calibration_1(s: str):
     n = len(s)
     first_found = False
     last_found = False
@@ -15,9 +19,59 @@ def find_calibration(s: str):
             result += int(last)
     return result
 
+def find_calibration_2(s: str):
+    n = len(s)
+    first_found = False
+    last_found = False
+    result = 0
+
+    def check_literal_forward(s, i) -> int:
+        for n in range(len(literal_numbers)):
+            lit = literal_numbers[n]
+            sl = s[i:i+len(lit)]
+            if sl == lit:
+                return literal_numbers_values[n]
+        return 0
+
+    def check_literal_backwards(s, i) -> int:
+        for n in range(len(literal_numbers)):
+            lit = literal_numbers[n]
+            sl = s[i - len(lit):i]
+            if sl == lit:
+                return literal_numbers_values[n]
+        return 0
+        
+    for i in range(n):
+        first = s[i]
+        last = s[n - i - 1]
+        forward = check_literal_forward(s, i)
+        backward = check_literal_backwards(s, n - i)
+        if (not first_found) and (first.isdigit() or forward):
+            first_found = True
+            if forward:
+                result += 10 * forward
+            else:
+                result += 10 * int(first)
+        if (not last_found) and (last.isdigit() or backward):
+            last_found = True
+            if backward:
+                result += backward
+            else:
+                result += int(last)
+    return result
+
+
+with open("../data/day-01.txt", "r") as f:
+    lines = f.readlines()
 
 total = 0
-for s in ["1abc2", "qr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]:
-    calib = find_calibration(s)
+for s in lines:
+    calib = find_calibration_1(s)
     total += calib
-    print(s, calib, total)
+print("Calibration 1: ", total)
+
+total = 0
+for s in lines:
+    calib = find_calibration_2(s)
+    total += calib
+print("Calibration 2: ", total)
