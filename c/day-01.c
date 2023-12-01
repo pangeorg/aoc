2916cb04edf3ea49c8208c005cbcb855d29f5820
Georg Stockinger <gstockinger@a2mac1.com>
2023-12-01 13:50:15 +0100

    Day 1: full c solution (ignore the file)

--- c/day-01.c
+++ c/day-01.c
@@ -3,38 +3,113 @@
 #include <stdlib.h>
 #include <string.h>
 
-int find_calibration(const char* str, int len) {
-  int found_first = 0;
-  int found_last = 0;
-  int result = 0;
+const char *literal_numbers[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+const int literal_numbers_value[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-  for (int i = 0; i < len; ++i) {
-    const char *first = &str[i];
-    const char *last = &str[len - i - 1];
+int find_calibration_1(const char *str, int len)
+{
+  int *buffer = calloc(len, sizeof(int));
+  int pos = 0;
 
-    if (isdigit(*first) && found_first == 0) {
-      found_first = 1;
-      result += 10 * atoi(first);
-    }
-    if (isdigit(*last) && found_last == 0) {
-      found_last = 1;
-      result += atoi(last);
+  for (int i = 0; i < len; ++i)
+  {
+    const char s = str[i];
+    if (isdigit(s))
+    {
+      buffer[pos] = atoi(&s);
+      pos++;
     }
   }
+
+  int result = buffer[0] * 10 + buffer[pos - 1];
+  free(buffer);
   return result;
 }
 
-int main() {
+void slice(const char *str, char *result, size_t start, size_t end)
+{
+  strncpy(result, str + start, end - start);
+}
 
-  const char *list[4] = {"1abc2", "pqr4stu8vwx", "a1b2c3d4e5f", "treb7uchet"};
+int find_calibration_2(const char *str, int len)
+{
+  int *buffer = malloc(len * sizeof(int));
+  char *sbuffer = malloc(len * sizeof(char));
+  int pos = 0;
 
+  for (int i = 0; i < len; ++i)
+  {
+    const char s = str[i];
+    if (isdigit(s))
+    {
+      buffer[pos] = atoi(&s);
+      pos++;
+      continue;
+    }
+
+    for (int lni = 0; lni < 9; ++lni)
+    {
+      const char *num = literal_numbers[lni];
+      const int val = literal_numbers_value[lni];
+      int slen = strlen(num);
+
+      if (i + slen < len + 1)
+      {
+        memset(sbuffer, 0, len);
+        slice(str, sbuffer, i, i + slen);
+        if (strcmp(sbuffer, num) == 0)
+        {
+          buffer[pos] = val;
+          pos++;
+        }
+      }
+    }
+  }
+
+  int result = buffer[0] * 10 + buffer[pos - 1];
+  free(buffer);
+  free(sbuffer);
+  return result;
+}
+
+int main()
+{
   int total = 0;
-  for (int i = 0; i < 4; ++i){
-    const char *s = list[i];
-    int r = find_calibration(s, strlen(s) - 1);
+
+  printf("\n#### CALIBRATION 1 #####\n");
+  const char *list1[4] = {
+    "1abc2",
+    "pqr4stu8vwx",
+    "a1b2c3d4e5f",
+    "treb7uchet"
+  };
+
+  for (int i = 0; i < 4; ++i)
+  {
+    const char *s = list1[i];
+    int r = find_calibration_1(s, strlen(s));
     total += r;
     printf("%s %d %d\n", s, r, total);
   }
 
+  printf("\n#### CALIBRATION 2 #####\n");
+  const char *list2[7] = {
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen",
+  };
+
+  total = 0;
+  for (int i = 0; i < 7; ++i)
+  {
+    const char *s = list2[i];
+    int r = find_calibration_2(s, strlen(s));
+    total += r;
+    printf("%s %d %d\n", s, r, total);
+  }
   return 0;
 }
