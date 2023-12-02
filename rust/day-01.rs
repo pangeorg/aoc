2483975cd2af7c5c9175f4372e549d3e27b038f6
Georg Stockinger <gstockinger@a2mac1.com>
2023-12-02 19:37:30 +0100

    day 1 part 1 rust and makefiles!!

--- rust/day-01.rs
+++ rust/day-01.rs
@@ -1,13 +1,31 @@
-use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+use std::io::{self};
+use std::fs::read_to_string;
+
+fn read_lines(filename: &str) -> Vec<String> {
+    let mut result = Vec::new();
+    for line in read_to_string(filename).unwrap().lines() {
+        result.push(line.to_string())
+    }
+    result
+}
+
+fn solve_1(lines: Vec<String>) -> u32 {
+    let mut result = Vec::new();
+    for line in lines {
+        for c in line.chars() {
+            if c.is_digit(10) {
+                let i:u32 = c.to_digit(10).unwrap();
+                result.push(i);
+            }
+        }
+    }
+    result.first().unwrap() * 10 + result.last().unwrap()
+}
 
 fn main() -> io::Result<()> {
-    let file = File::open("../data/day-01.txt")?;
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        println!("{}", line?);
-    }
+    let lines = read_lines("../data/day-01.txt");
+    let result1 = solve_1(lines);
+    println!("Day 01 - Result 1: {}", result1);
 
     Ok(())
 }
